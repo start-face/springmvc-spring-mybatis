b905c9a62dd37dcfac35a88ca5c5ec78bba199fe
FaceFeel <995956062@qq.com>
2018-04-27 15:08:47 +0800

    临时提交

---
+++ src/main/java/com/ssm/controller/RootController.java
@@ -0,0 +1,38 @@
+package com.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author FaceFeel
+ * @Created 2018-04-27 11:21
+ **/
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+    @RequestMapping("login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("index")
+    public String index(HttpServletRequest request){
+
+        //在这判断登录
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String parameter = request.getParameter("");
+        System.err.println("userName:********" + username);
+        System.err.println("passWord:********" + password);
+
+        if (!"".equals(username) && !"".equals(password)){
+            //登录成功跳转后台
+            return "index";
+        }
+        //登录失败跳回原来的页面
+        return "redirect:/login";
+    }
+}
